3130abde8c7af5a19b579b6e51f5dc48d283d637
Mike <60325499+leguim-repo@users.noreply.github.com>
2022-06-05 20:36:28 +0200

    Same code for all lambdas

--- app/etls/lambda_dummy/src/lambda_dummy.py
+++ app/etls/lambda_dummy/src/lambda_dummy.py
@@ -3,7 +3,8 @@ import os
 
 
 def lambda_handler(event, context):
-    print(os.environ)
+    print(f"event:\n{json.dumps(event)}")
+    print(f"os.environ:\n{os.environ}")
     return {
         "status_code": 200,
         "lambda_name": __name__,
--- app/sns/lambda_sns_subscriber/src/lambda_sns_subscriber.py
+++ app/sns/lambda_sns_subscriber/src/lambda_sns_subscriber.py
@@ -1,8 +1,12 @@
 import json
+import os
 
 
 def lambda_handler(event, context):
-    print(f"event: {json.dumps(event)}")
-    return {"status_code": 200,
-            "lambda_name": __name__,
-            "event:": json.dumps(event)}
+    print(f"event:\n{json.dumps(event)}")
+    print(f"os.environ:\n{os.environ}")
+    return {
+        "status_code": 200,
+        "lambda_name": __name__,
+        "event": json.dumps(event),
+    }
