98b9825ed6029525d270e62b39510a25c8c84340
Mike <60325499+leguim-repo@users.noreply.github.com>
2022-06-12 20:17:16 +0200

    Added log retention policy to etls lambdas

--- infrastructure/modules/etls/main.tf
+++ infrastructure/modules/etls/main.tf
@@ -3,9 +3,8 @@ data "archive_file" "zip" {
   source_file = "../app/etls/lambda_dummy/src/lambda_dummy.py"
   output_path = "lambda_dummy.zip"
 }
-
 resource "aws_lambda_function" "lambda_dummy" {
-  function_name                  = "lambda_dummy"
+  function_name                  = "lambda_dummy-${var.project_name}-${var.environment}"
   role                           = var.lambda_role_execution
   tags                           = var.tags
   handler                        = "lambda_dummy.lambda_handler"
@@ -24,6 +23,14 @@ resource "aws_lambda_function" "lambda_dummy" {
     }
   }
 }
+resource "aws_cloudwatch_log_group" "lambda_dummy" {
+  retention_in_days = 7
+  tags              = var.tags
+  lifecycle {
+    create_before_destroy = true
+    prevent_destroy       = false
+  }
+}
 
 
 data "archive_file" "lambda_intake_zip" {
@@ -31,9 +38,8 @@ data "archive_file" "lambda_intake_zip" {
   source_file = "../app/etls/lambda_intake/src/lambda_intake.py"
   output_path = "lambda_intake.zip"
 }
-
 resource "aws_lambda_function" "lambda_intake" {
-  function_name                  = "lambda_intake"
+  function_name                  = "lambda_intake-${var.project_name}-${var.environment}"
   role                           = var.lambda_role_execution
   tags                           = var.tags
   handler                        = "lambda_intake.lambda_handler"
@@ -46,13 +52,19 @@ resource "aws_lambda_function" "lambda_intake" {
   filename         = data.archive_file.lambda_intake_zip.output_path
   source_code_hash = data.archive_file.lambda_intake_zip.output_base64sha256
 
-
 }
-
+resource "aws_cloudwatch_log_group" "lambda_intake" {
+  retention_in_days = 7
+  tags              = var.tags
+  lifecycle {
+    create_before_destroy = true
+    prevent_destroy       = false
+  }
+}
 ##### S3 Notifications
 ## https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_notification
 
-## Lambda on file upload
+## Lambda triggered when file upload
 resource "aws_s3_bucket_notification" "s3_lambda_on_upload" {
   bucket = var.intake_bucket
 
@@ -62,5 +74,4 @@ resource "aws_s3_bucket_notification" "s3_lambda_on_upload" {
     filter_prefix       = "pictures/"
     filter_suffix       = ".jpeg"
   }
-
 }
--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -11,6 +11,7 @@ provider "aws" {
     apigateway       = "http://localhost:4566"
     cloudformation   = "http://localhost:4566"
     cloudwatch       = "http://localhost:4566"
+    cloudwatchlogs   = "http://localhost:4566"
     cloudwatchevents = "http://localhost:4566"
     dynamodb         = "http://localhost:4566"
     ec2              = "http://localhost:4566"
